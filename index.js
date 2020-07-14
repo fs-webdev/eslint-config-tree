726691d7508f44c12ea81f51753f649028d317c1
Zachary Williams <zlw11063@gmail.com>
2020-07-14 12:16:05 -0600

    swap back order of plugins

--- index.js
+++ index.js
@@ -55,8 +55,8 @@ module.exports = {
     // 'eslint-plugin-bestpractices',
     // 'eslint-plugin-deprecate',
     'eslint-plugin-html',
-    'eslint-plugin-json',
-    'eslint-plugin-jsdoc'
+    'eslint-plugin-jsdoc',
+    'eslint-plugin-json'
     // 'eslint-plugin-no-only-tests',
     // 'eslint-plugin-no-skip-tests',
     // 'eslint-plugin-promise',
