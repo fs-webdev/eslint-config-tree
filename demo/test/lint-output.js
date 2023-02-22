e9220ec12bedc463df294776058e78c658d527c9
Jordi Kloosterboer <vetaz@familysearch.org>
2023-02-22 16:05:13 -0700

    Compare Final Config for testing

--- demo/test/lint-output.js
+++ demo/test/lint-output.js
@@ -1,15 +1,23 @@
 import test from 'ava';
 const fileManager = require('file-manager-js');
 
-test('Should have consistent rule output', async t => {
+function processFile (t, filename) {
   // Run previously via npm test, save off results, and read output
-  return fileManager.readFile('./demo/test/snapshots/new-lint-results.txt')
+  return fileManager.readFile(`./demo/test/snapshots/${filename}`)
     .then((content) => {
-      const eslintOutput = content.toString(); // content is instance of Buffer, so it needs to be parsed
-      return t.snapshot(eslintOutput);
+      const output = content.toString(); // content is instance of Buffer, so it needs to be parsed
+      return t.snapshot(output);
     })
     .catch((err) => {
       console.log(err);
       return t.fail();
     });
+}
+
+test('Should have consistent rule output', async t => {
+  return processFile(t, 'new-lint-results.txt');
+});
+
+test('Should have consistent eslint final config', async t => {
+  return processFile(t, 'new-lint-final-config.json');
 });
