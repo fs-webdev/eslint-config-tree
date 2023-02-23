f6abd0644b9a89a0590f15b3114b5b24b3af7269
Jordi Kloosterboer <vetaz@familysearch.org>
2023-02-22 18:36:04 -0700

    extend react and es6 configs

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const dataTestId = 'data-testid'
 
 module.exports = {
   env: { es2021: true, browser: true, mocha: true },
-  extends: ['@fs/eslint-config-frontier-react/prettierSetup'],
+  extends: [
+    '@fs/eslint-config-frontier-react/react',
+    '@fs/eslint-config-frontier-react/es6',
+    '@fs/eslint-config-frontier-react/prettierSetup',
+  ],
   parser: '@babel/eslint-parser',
   parserOptions: {
     ecmaVersion: 12,
