44851629f65b9b295d90a2a5c3394fc533d85b97
Jordi Kloosterboer <vetaz@familysearch.org>
2023-02-22 18:51:49 -0700

    Use frontiers json config
    Differences:
    eslint won't complain on json files if there is a comment (good so we don't get eslint failing on a json file that actually does allow comments like some do (eslintrc.json is an example))
    it still says what the issue is, but it doesn't specify the exact json rule because they are all combined into json/*
    
    I think this is fine.

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ module.exports = {
   extends: [
     '@fs/eslint-config-frontier-react/react',
     '@fs/eslint-config-frontier-react/es6',
+    '@fs/eslint-config-frontier-react/json',
     '@fs/eslint-config-frontier-react/jest',
     '@fs/eslint-config-frontier-react/dont-need-lodash',
     '@fs/eslint-config-frontier-react/prettierSetup',
@@ -16,18 +17,7 @@ module.exports = {
     sourceType: 'module',
     requireConfigFile: false,
   },
-  plugins: [
-    'bestpractices',
-    'deprecate',
-    'html',
-    'import',
-    'jsdoc',
-    'json',
-    'node',
-    'promise',
-    'sonarjs',
-    'test-selectors',
-  ],
+  plugins: ['bestpractices', 'deprecate', 'html', 'import', 'jsdoc', 'node', 'promise', 'sonarjs', 'test-selectors'],
   /**
    * @property {object} rules - Tree custom rule and additional linter configuration.
    */
@@ -135,26 +125,6 @@ module.exports = {
     'jsdoc/require-returns': 'warn',
     'jsdoc/require-throws': 'off',
     'jsdoc/valid-types': 'warn',
-
-    'json/undefined': 'error',
-    'json/enum-value-mismatch': 'error',
-    'json/unexpected-end-of-comment': 'error',
-    'json/unexpected-end-of-string': 'error',
-    'json/unexpected-end-of-number': 'error',
-    'json/invalid-unicode': 'error',
-    'json/invalid-escape-character': 'error',
-    'json/invalid-character': 'error',
-    'json/property-expected': 'error',
-    'json/comma-expected': 'error',
-    'json/colon-expected': 'error',
-    'json/value-expected': 'error',
-    'json/comma-or-close-backet-expected': 'error',
-    'json/comma-or-close-brace-expected': 'error',
-    'json/trailing-comma': 'error',
-    'json/duplicate-key': 'error',
-    'json/comment-not-permitted': 'error',
-    'json/schema-resolve-error': 'error',
-    'json/unknown': 'error',
 
     'jsx-quotes': 'off',
 
