7b390f0b5f827ba53aab4f7ad0546e9c440ce12c
Jordi Kloosterboer <vetaz@familysearch.org>
2023-02-22 22:50:59 -0700

    Make it easier to see differences in md file
    This 2 space is temp for this pr

--- format-config.js
+++ format-config.js
@@ -2,11 +2,19 @@
 const finalConfig = require('./demo/test/snapshots/new-lint-final-config.json');
 const FS = require('fs');
 
-const formattedRules = Object.fromEntries(Object.entries(finalConfig?.rules ?? {}).sort(([ruleNameA], [ruleNameB]) => {
-  if (ruleNameA > ruleNameB) return 1;
-  if (ruleNameB > ruleNameA) return -1;
-  return 0;
-}));
+const formattedRules = Object.fromEntries(
+  Object.entries(finalConfig?.rules ?? {}).sort(([ruleNameA], [ruleNameB]) => {
+    if (ruleNameA > ruleNameB) return 1;
+    if (ruleNameB > ruleNameA) return -1;
+    return 0;
+  })
+);
 
-// eslint-disable-next-line no-console -- We want to have the console.log here.
-FS.writeFile('./demo/test/snapshots/new-lint-final-config.json', JSON.stringify({ ...finalConfig, rules: formattedRules }), (err) => { if (err) console.log('There was an error writing to new-lint-final-config.json file:', err); });
+FS.writeFile(
+  './demo/test/snapshots/new-lint-final-config.json',
+  JSON.stringify({ ...finalConfig, rules: formattedRules }, null, 2),
+  (err) => {
+    // eslint-disable-next-line no-console -- We want to have the console.log here.
+    if (err) console.log('There was an error writing to new-lint-final-config.json file:', err);
+  }
+);
