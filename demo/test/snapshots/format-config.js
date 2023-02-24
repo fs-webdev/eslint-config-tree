2f84e80e71d828b59d128b70a10be26fcd5da7dc
Jordi Kloosterboer <vetaz@familysearch.org>
2023-02-23 19:36:33 -0700

    Don't use sed

--- demo/test/snapshots/format-config.js
+++ demo/test/snapshots/format-config.js
@@ -1,15 +1,18 @@
 // Process the final eslint config file:
 // Sort rules alphabetically to compare changes easier.
-// Remove any developer-specific directory paths.
+// Remove any developer-specific directory paths for that and the eslint error/warning output
 
 const finalConfig = require('./local-linting-final-config.json');
+
 const FS = require('fs');
 
-const formattedRules = Object.fromEntries(Object.entries(finalConfig?.rules ?? {}).sort(([ruleNameA], [ruleNameB]) => {
-  if (ruleNameA > ruleNameB) return 1;
-  if (ruleNameB > ruleNameA) return -1;
-  return 0;
-}));
+const formattedRules = Object.fromEntries(
+  Object.entries(finalConfig?.rules ?? {}).sort(([ruleNameA], [ruleNameB]) => {
+    if (ruleNameA > ruleNameB) return 1;
+    if (ruleNameB > ruleNameA) return -1;
+    return 0;
+  })
+);
 
 FS.writeFile(
   './demo/test/snapshots/local-linting-final-config.json',
@@ -22,3 +25,17 @@ FS.writeFile(
     if (err) console.log('There was an error writing to local-linting-final-config.json file:', err); // eslint-disable-line no-console -- Node scripts use the console.
   }
 );
+
+FS.readFile('./demo/test/snapshots/local-linting-output.txt', 'utf8', (err, eslintOutput) => {
+  if (err) {
+    console.log('There was an error reading local-linting-output.txt', err); // eslint-disable-line no-console -- Node scripts use the console.
+  } else {
+    FS.writeFile(
+      './demo/test/snapshots/local-linting-output.txt',
+      eslintOutput.replaceAll(/.*demo\//g, ''),
+      (err2) => {
+        if (err2) console.log('There was an error writing to local-linting-output.txt file:', err); // eslint-disable-line no-console -- Node scripts use the console.
+      }
+    );
+  }
+});
