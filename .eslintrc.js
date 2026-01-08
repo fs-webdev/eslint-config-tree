f81087dc71b7854c28b6fe301715b6547141ff7f
Jordi Kloosterboer <vetaz@familysearch.org>
2026-01-08 16:50:06 -0700

    Update ava

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,14 @@
-module.exports = { extends: ['./index.js'], settings: { jest: { version: 29 } } }
+module.exports = {
+  extends: ['./index.js'],
+  settings: { jest: { version: 29 } },
+  overrides: [
+    {
+      files: ['demo/test/**/*.js'],
+      rules: {
+        // Allow importing devDependencies in test files without requiring them to be resolvable
+        // This is needed because AVA v6 uses modern package.json exports that older resolvers don't understand
+        'import/no-unresolved': ['error', { ignore: ['^ava$'] }],
+      },
+    },
+  ],
+}
